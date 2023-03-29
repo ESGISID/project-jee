a9304309fc49812b9f5c73215cce1b5ca1a7ddba
Sid B <sid.bennaceur@haidi.io>
2023-03-29 17:41:45 +0200

    remove non used files

--- FillZipCodes/src/main/java/com/example/fillzipcodes/JsonZipList.java
+++
@@ -1,7 +0,0 @@
-package com.example.fillzipcodes;
-
-import java.util.List;
-
-public class JsonZipList {
-    public List<JsonZips> zipsList;
-}
--- FillZipCodes/src/main/java/com/example/fillzipcodes/Wesh.java
+++
@@ -1,7 +0,0 @@
-package com.example.fillzipcodes;
-
-import java.io.IOException;
-
-public interface Wesh {
-    void yoyoyo() throws IOException;
-}
